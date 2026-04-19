09480388223afbc337a208431870fc1f2fbe1b44
vipin <vipinzingade01@gmail.com>
2026-04-19 13:57:07 +0530

    Configure for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/smart-personal-finanace-tracker/',
+    base: '/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
